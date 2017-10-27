b62be575df50f39131765349a956aa38b06ced98
Sohailbasha <shlbsh94@gmail.com>
2017-10-26 23:32:48 -0400

    editing model object to use Sources over category type

--- Newsly/Model/Category.swift
+++ Newsly/Model/Category.swift
@@ -20,12 +20,21 @@ class Category {
 }
 
 enum CategoryType: String {
-
     case technology = "technology"
     case gaming = "gaming"
     case entertainment = "entertainment"
     case business = "business"
     case science = "science-and-nature"
     case sports = "sport"
-    
+}
+
+enum Sources: String {
+    case tech = "the-next-web,the-verge,engadget,crypto-coins-news,hacker-news,wired"
+    case gaming = "ign,polygon"
+    case business = "the-wall-street-journal,fortune,business-insider,financial-post"
+    case science = "national-geographic,new-scientist"
+    case sports = "bbc-sport,bleacher-report,talksport,espn"
+    case popCulture = "the-lad-bible,buzzfeed,mashable,reddit-r-all"
+    case international = "al-jazeera-english,news24,the-times-of-india,rte"
+    case reputedSources = "time,the-washington-post,the-new-york-times,reuters"
 }
